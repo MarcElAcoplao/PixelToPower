8d8d3c4ba88607e0dd5af0afb2a2f21d4e9da911
MarcElAcoplao <marcrams2007@gmail.com>
2024-12-17 20:00:05 +0100

    v0.0.0.1 costos, efectes, balanç i més millores

--- constants.js
+++ constants.js
@@ -2,10 +2,10 @@
 const generadors = [
   {nom: "test", cost: 0, quantitat: 0, efecte: 0},
   {nom: "gen1", cost: 1, quantitat: 0, efecte: 1},
-  {nom: "gen2", cost: 10, quantitat: 0, efecte: 2},
-  {nom: "gen3", cost: 100, quantitat: 0, efecte: 3},
-  {nom: "gen4", cost: 1000, quantitat: 0, efecte: 4},
-  {nom: "gen5", cost: 10000, quantitat: 0, efecte: 5},
+  {nom: "gen2", cost: 10, quantitat: 0, efecte: 5},
+  {nom: "gen3", cost: 100, quantitat: 0, efecte: 25},
+  {nom: "gen4", cost: 1000, quantitat: 0, efecte: 125},
+  {nom: "gen5", cost: 10000, quantitat: 0, efecte: 625},
 ]
 
 const millores = [
@@ -15,20 +15,26 @@ const millores = [
   {nom: "millora3", cost: 200, nombreRequisits: 1, requisit: 2, efecte: "x2 Vida per 200 diners"},
   {nom: "millora4", cost: 1000, nombreRequisits: 1, requisit: 3, efecte: "x4 Vida per 1000 diners"},
   {nom: "millora5", cost: 5000, nombreRequisits: 1, requisit: 4, efecte: "x5 Vida per 5000 diners"},
-  {nom: "millora6", cost: 20000, nombreRequisits: 1, requisit: 5, efecte: "x10 Vida per 20000 diners"},
+  {nom: "millora6", cost: 20000, nombreRequisits: 1, requisit: 5, efecte: "x10 Vida per 20k diners"},
   {nom: "millora7", cost: 50, nombreRequisits: 1, requisit: 1, efecte: "x1.25 Mal per 50 diners"},
   {nom: "millora8", cost: 200, nombreRequisits: 1, requisit: 7, efecte: "x2 Mal per 200 diners"},
   {nom: "millora9", cost: 1000, nombreRequisits: 1, requisit: 8, efecte: "x4 Mal per 1000 diners"},
   {nom: "millora10", cost: 5000, nombreRequisits: 1, requisit: 9, efecte: "x5 Mal per 5000 diners"},
-  {nom: "millora11", cost: 20000, nombreRequisits: 1, requisit: 10, efecte: "x10 Mal per 20000 diners"},
+  {nom: "millora11", cost: 20000, nombreRequisits: 1, requisit: 10, efecte: "x10 Mal per 20k diners"},
   {nom: "millora12", cost: 1000, nombreRequisits: 2, requisits: [3, 8], efecte: "Desbloquejar els nivells per 1000 diners"},
+  {nom: "millora13", cost: 2000, nombreRequisits: 1, requisit: 12, efecte: "x1.25 Monedes per 2000 diners"},
+  {nom: "millora14", cost: 10000, nombreRequisits: 1, requisit: 13, efecte: "x1.5 Monedes per 10k diners"},
+  {nom: "millora15", cost: 50000, nombreRequisits: 1, requisit: 14, efecte: "x2 Monedes per 50k diners"},
+  {nom: "millora16", cost: 2000, nombreRequisits: 1, requisit: 12, efecte: "x1.25 Exp per 2000 diners"},
+  {nom: "millora17", cost: 10000, nombreRequisits: 1, requisit: 16, efecte: "x1.5 Exp per 10k diners"},
+  {nom: "millora18", cost: 50000, nombreRequisits: 1, requisit: 17, efecte: "x2 Exp per 50k diners"},
 ]
 
 const enemics = [
-  {nom: "test", recompensa: 0, vida: 0.1, mal: 0.1, nivell: 1},
-  {nom: "basic", recompensa: 1, vida: 1, mal: 1, nivell: 1},
-  {nom: "basic 2", recompensa: 2, vida: 1.25, mal: 1.25, nivell: 2},
-  {nom: "basic 3", recompensa: 4, vida: 2, mal: 2, nivell: 3},
-  {nom: "basic 4", recompensa: 8, vida: 10, mal: 10, nivell: 4},
-  {nom: "basic 5", recompensa: 16, vida: 100, mal: 100, nivell: 5},
+  {nom: "test", recompensa: 0, vida: 0.1, mal: 0.1, nivell: 1, temps: 0, imatge: 0},
+  {nom: "Basic", recompensa: 1, vida: 1, mal: 1, nivell: 1, temps: 2, imatge: 1},
+  {nom: "Basic 2", recompensa: 2, vida: 1.5, mal: 1.5, nivell: 1, temps: 3, imatge: 1},
+  {nom: "Basic 3", recompensa: 4, vida: 5, mal: 5, nivell: 2, temps: 5, imatge: 1},
+  {nom: "Basic 4", recompensa: 8, vida: 40, mal: 40, nivell: 3, temps: 7, imatge: 1},
+  {nom: "Basic 5", recompensa: 16, vida: 300, mal: 300, nivell: 4, temps: 10, imatge: 1},
 ]
--- script.js
+++ script.js
@@ -8,8 +8,9 @@ function reset() {
     vida: 1,
     mal: 1,
     desbloqueig: 0,
-    generadors: [0,0,0,0,0],
-    efecteGeneradors: [1,2,3,4,5],
+    generadors: [0,0,0,0,0,0],
+    costGeneradors: [0,1,10,100,1000,10000],
+    efecteGeneradors: [0,1,5,25,125,625],
     produccioGeneradors: 0,
     milloresComprades: [1,0,0,0,0,0],
     ultimaGeneracio: Date.now(),
@@ -18,6 +19,9 @@ function reset() {
     potBaixar: 0,
     exp: 0,
     nivell: 1,
+    tempsLluita: 0,
+    monedesMulti: 1,
+    expMulti: 1,
   }
 }
 reset()
@@ -99,9 +103,10 @@ function nombreEntrades() {
 }
 
 function comprarGenerador(x) {
-  if (game.monedes >= generadors[x].cost) {
-    game.monedes -= generadors[x].cost
-    generadors[x].quantitat++
+  if (game.monedes >= game.costGeneradors[x]) {
+    game.monedes -= game.costGeneradors[x]
+    game.generadors[x]++
+    game.costGeneradors[x] = formulaCost(x)
   }
 }
 
@@ -116,9 +121,9 @@ function textGeneradors() { //Actualitza el text dels generadors i si els podem 
   if (game.pantalla == 1)
   for (i=1;i<generadors.length;i++) {
     document.getElementById(generadors[i].nom).style.display = "block"
-    if (game.monedes < generadors[i].cost) document.getElementById(generadors[i].nom).disabled = true
+    if (game.monedes < game.costGeneradors[i]) document.getElementById(generadors[i].nom).disabled = true
     else document.getElementById(generadors[i].nom).disabled = false
-    document.getElementById(generadors[i].nom).innerHTML = "Tens " + generadors[i].quantitat + " Generadors " + i + " amb producció total de " + (generadors[i].quantitat * generadors[i].efecte) + "/s<br>Cost: " + generadors[i].cost + " Monedes"
+    document.getElementById(generadors[i].nom).innerHTML = "Tens " + numberShort(game.generadors[i]) + " Generadors " + i + " amb producció total de " + numberShort(game.generadors[i] * game.efecteGeneradors[i]) + "/s<br>Cost: " + numberShort(game.costGeneradors[i]) + " Monedes"
   }
   else {for (i=1;i<generadors.length;i++) {document.getElementById(generadors[i].nom).style.display = "none"}
  }
@@ -144,6 +149,14 @@ setInterval(textMillores, 50)
 function textEnemics() {
   if (game.pantalla == 3) {
   document.getElementById("enemic").style.display = "block"
+  if (game.tempsLluita == 0) {
+    document.getElementById("enemic").disabled = false
+    document.getElementById("enemic").innerHTML = "Lluitar amb un enemic " + enemics[game.enemic].nom
+  } 
+  else {
+    document.getElementById("enemic").disabled = true
+    document.getElementById("enemic").innerHTML = "Temps per a lluitar: " + numberToTime(game.tempsLluita)
+  }
   document.getElementById("enemicAmunt").style.display = "block"
   if (game.potPujar == 1) {document.getElementById("enemicAmunt").disabled = false} else {document.getElementById("enemicAmunt").disabled = true}
   document.getElementById("enemicAvall").style.display = "block"
@@ -178,19 +191,12 @@ function milloraDisponible(x) {
  }
 }
 
-function assignarValors() { //Jo entenc lo que això fa...
-  for (i=1;i<generadors.length;i++) {
-    if (game.generadors[i-1] < generadors[i].quantitat) game.generadors[i-1] = generadors[i].quantitat
-    else generadors[i].quantitat = game.generadors[i-1]
-  }
-}
-setInterval(assignarValors, 50)
-
 function generarDiners() { //S'activa 20 vegades per segon i determina la quantitat produida respecte la última vegada, per evitar perdre producció al tancar la pàgina
   let producció = 0
-  for (i=1;i<generadors.length;i++) {producció += (generadors[i].quantitat * generadors[i].efecte)}
+  for (i=1;i<generadors.length;i++) {producció += (game.generadors[i] * game.efecteGeneradors[i])}
   game.diners += producció * ((Date.now() - game.ultimaGeneracio)/1000)
   game.produccioGeneradors = producció
+  if (game.tempsLluita > 0) {game.tempsLluita -= ((Date.now() - game.ultimaGeneracio)/1000)} else {game.tempsLluita = 0}
   game.ultimaGeneracio = Date.now()
 }
 setInterval(generarDiners, 50)
@@ -198,7 +204,7 @@ setInterval(generarDiners, 50)
 function textInfo() { //Actualitza el text dels diferents elements
   document.getElementById("nextUnlockLevel").innerHTML = "Hola a tothom"
   document.getElementById("selectedPetText").innerHTML = "<img src='img/shop/23.png' style='width: 128px' onerror=\"this.onerror=null;this.src='img/enemies/0.png';\"><br>" + stats(game.desbloqueig)
-  document.getElementById("enemyImage").innerHTML = "<img src='img/enemies/" + game.enemic + ".png' style='width: 128px' onerror=\"this.onerror=null;this.src='img/enemies/0.png';\"><br>"
+  document.getElementById("enemyImage").innerHTML = "<img src='img/enemies/" + enemics[game.enemic].imatge + ".png' style='width: 128px' onerror=\"this.onerror=null;this.src='img/enemies/0.png';\"><br>Vida: " + numberShort(enemics[game.enemic].vida) + "<br>Mal: " + numberShort(enemics[game.enemic].mal) + "<br>Recompensa: " + numberShort(enemics[game.enemic].recompensa)
 }
 setInterval(textInfo, 50)
 
@@ -210,6 +216,10 @@ function stats(x) { //Retorna tots els possibles valors per als atributs
     result += "Monedes: " + numberShort(game.monedes) + "<br>"
     result += "Vida: " + numberShort(game.vida) + "<br>"
     result += "Mal: " + numberShort(game.mal) + "<br>"
+  }
+  if (x >= 2) {
+    result += "Monedes x" + numberShort(game.monedesMulti) + "<br>"
+    result += "Exp x" + numberShort(game.expMulti) + "<br>"
   }
 
   return result
@@ -223,7 +233,9 @@ setInterval(desbloqueig, 50)
 function efectesMillores() {
   game.desbloqueig = 0 + Math.min(game.milloresComprades[1], 1) + Math.min(game.milloresComprades[12], 1)
   game.vida = 1 * (game.milloresComprades[2] * 0.25 + 1) * (game.milloresComprades[3] * 1 + 1) * (game.milloresComprades[4] * 3 + 1) * (game.milloresComprades[5] * 4 + 1) * (game.milloresComprades[6] * 9 + 1)
-  game.mal = 1 * (game.milloresComprades[7] * 0.25 + 1) * (game.milloresComprades[8] * 1 + 1) * (game.milloresComprades[9] * 3 + 1) * (game.milloresComprades[10] * 4 + 1) * (game.milloresComprades[11] * 9 + 1) 
+  game.mal = 1 * (game.milloresComprades[7] * 0.25 + 1) * (game.milloresComprades[8] * 1 + 1) * (game.milloresComprades[9] * 3 + 1) * (game.milloresComprades[10] * 4 + 1) * (game.milloresComprades[11] * 9 + 1)
+  game.monedesMulti = 1 * (game.milloresComprades[13] * 0.25 + 1) * (game.milloresComprades[14] * 0.5 + 1) * (game.milloresComprades[15] * 1 + 1)
+  game.expMulti = 1 * (game.milloresComprades[16] * 0.25 + 1) * (game.milloresComprades[17] * 0.5 + 1) * (game.milloresComprades[18] * 1 + 1)
 }
 setInterval(efectesMillores, 50)
 
@@ -231,9 +243,10 @@ function lluitar(x) {
    let copsGuanyar = enemics[x].vida / game.mal 
    let copsPerdre = game.vida / enemics[x].mal
    if (copsGuanyar <= copsPerdre) {
-    game.monedes += enemics[x].recompensa
-    if (game.desbloqueig >= 2) game.exp += enemics[x].recompensa
+    game.monedes += enemics[x].recompensa * game.monedesMulti
+    if (game.desbloqueig >= 2) game.exp += enemics[x].recompensa * game.expMulti
   }
+  game.tempsLluita = enemics[x].temps
 }
 
 function nivellEnemic(x) { //1 - Pujar, 2 - Baixar
@@ -249,8 +262,13 @@ function canviEnemic() {
 }
 setInterval(canviEnemic, 50)
 
-function XPToLevel(x) {return Math.floor((x / 5) ** 0.4) + 1}
-function levelToXP(x) {return Math.ceil((x-1) ** (1/0.4) * 5)}
+function XPToLevel(x) {return Math.floor((x / 5) ** 0.25) + 1}
+function levelToXP(x) {return Math.ceil((x-1) ** (1/0.25) * 5)}
+function formulaCost(x) {
+  let base = generadors[x].cost
+  let quantitat = game.generadors[x]
+  return ((base) * (1 + (quantitat)/5 + (quantitat ** 2)/25)) 
+}
 
 function nivells() {
   game.nivell = XPToLevel(Math.max(Math.floor(game.exp), 0))
@@ -276,6 +294,22 @@ else if (x == 0) result = x
 else if (x < 1 && x > -1) result = (x).toFixed(3)
 else result = (x).toFixed(2)
 return result
+  }
+}
+
+function numberToTime(x) {
+  if (typeof x === 'number' && !isNaN(x)) {
+  xCeil = Math.ceil(x)
+  result = ""
+  if (xCeil>=172800) result += Math.floor(xCeil/86400) + " dies "
+  else if (xCeil>=86400) result += Math.floor(xCeil/86400) + " dia "
+  if (Math.floor(xCeil/3600)%24 == 1) result += (Math.floor(xCeil/3600)%24) + " hora "
+  else if (Math.floor(xCeil/3600)%24 != 0) result += (Math.floor(xCeil/3600)%24) + " hores "
+  if (Math.floor(xCeil/60)%60 == 1) result += (Math.floor(xCeil/60)%60) + " minut "
+  else if (Math.floor(xCeil/60)%60 != 0) result += (Math.floor(xCeil/60)%60) + " minuts "
+  if (xCeil%60 == 1) result += Math.floor(xCeil%60) + " segon "
+  else if (xCeil%60 != 0) result += Math.floor(xCeil%60) + " segons "
+  return result
   }
 }
 
